1d57b4e69c5edb62ca87df29288372d47a29d6cc
Md Masum Ali <masumalihstu18@gmail.com>
2022-03-18 15:55:39 +0600

    class2

--- main/class2.dart
+++ main/class2.dart
@@ -1,20 +1,20 @@
 void main() {
-  // bool value = true;
-  // var myMap = {
-  //   "name": "Masum",
-  //   "age": 24,
-  //   "gender": value == true ? "male" : "female"
-  // };
-  // print(myMap);
-
-  var mySet = {
-    "masum",
-    "rimon",
-    "razul",
-    "akkas",
-    "bappy",
-    "fahad",
-    for (int i = 0; i <= 10; i++) "E$i"
+  bool value = true;
+  var myMap = {
+    "name": "Masum",
+    "age": 24,
+    "gender": value == true ? "male" : "female"
   };
-  print(mySet);
+  print(myMap);
+
+  // var mySet = {
+  //   "masum",
+  //   "rimon",
+  //   "razul",
+  //   "akkas",
+  //   "bappy",
+  //   "fahad",
+  //   for (int i = 0; i <= 10; i++) "E$i"
+  // };
+  // print(mySet);
 }
